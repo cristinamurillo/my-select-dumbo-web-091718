2f7158fe448f5b1714a02866091e7fc25953f4d7
Cristina Murillo <cmurill1@jhu.edu>
2018-08-16 17:56:31 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ new_array=[]
+ if block_given?
+   i = 0 
+   while i < collection.size 
+     if (yield collection[i]) == true 
+       new_array << collection[i]
+     end 
+     i+=1 
+   end
+ end
+ new_array
 end
