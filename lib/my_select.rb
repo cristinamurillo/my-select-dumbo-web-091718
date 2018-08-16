6452ba04013f768c4e7c8ecc64e91c85b19ba8a6
Cristina Murillo <cmurill1@jhu.edu>
2018-08-16 17:49:35 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,11 @@
 def my_select(collection)
- 
+ new_array=[]
  if block_given?
    i = 0 
    while i < collection.size 
-     yield array[i]
+     yield collection[i]
      i+=1 
    end
  end
+ new_array
 end
